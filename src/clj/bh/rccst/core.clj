7d1b63a83f6a7015b842af44215e2913000d0c8e
Chris Wasser <cawasser@mac.com>
2021-12-23 10:30:01 -0500

    - refactored nRepl into its own namespace

--- src/clj/bh/rccst/core.clj
+++ src/clj/bh/rccst/core.clj
@@ -2,12 +2,12 @@
   (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
             [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
-            [clojure.tools.nrepl.server :as nrepl]
 
             [bh.rccst.webserver :as server]
             [bh.rccst.websocket :as socket]
             [bh.rccst.broadcast :as broadcast]
             [bh.rccst.system :as system]
+            [bh.rccst.repl :as repl]
             
             [bh.rccst.publish]))
 
@@ -20,7 +20,7 @@
   (let []
     (component/system-map
       :server (component/using (server/map->HTTPServer args) [:socket])
-      :nrepl (nrepl/start-server :port (:nrepl args))
+      :nrepl (repl/start-repl args)
       :socket (socket/map->WebSocketServer args)
       :broadcast (component/using (broadcast/map->Broadcast args) [:socket]))))
 
@@ -40,9 +40,6 @@
 (defn -main [& args]
   (log/info "This is the main entrypoint!")
   (start!))
-
-
-
 
 
 ; run things from the REPL
@@ -71,5 +68,17 @@
 
 
   (socket/start-example-broadcaster! (:socket system))
+
+  ())
+
+
+; using the jack-in Repl
+(comment
+  @system/system
+
+  (:server @system/system)
+  (:nrepl @system/system)
+  (:socket @system/system)
+  (:broadcast @system/system)
 
   ())
---
+++ src/clj/bh/rccst/repl.clj
@@ -0,0 +1,10 @@
+(ns bh.rccst.repl
+  (:require [clojure.tools.logging :as log]
+            [clojure.tools.nrepl.server :as nrepl]))
+
+
+(defn start-repl [args]
+  (let [port (:nrepl args)]
+    (log/info "Starting nRepl at" port)
+    (println "Starting nRepl at" port)
+    (nrepl/start-server :port port)))
