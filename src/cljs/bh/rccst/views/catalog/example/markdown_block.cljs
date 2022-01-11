a3f0aae8f42eb83a77abfe63df4e1c08f118b935
Chris Wasser <cawasser@mac.com>
2022-01-11 17:10:15 -0500

    - got some markdown working (version issues with the react-markdown component)

--- src/cljs/bh/rccst/views/catalog/example/markdown_block.cljs
+++ src/cljs/bh/rccst/views/catalog/example/markdown_block.cljs
@@ -1,10 +1,23 @@
 (ns bh.rccst.views.catalog.example.markdown-block
   (:require [woolybear.ad.catalog.utils :as acu]
-            [woolybear.ad.layout :as layout]))
+            [woolybear.ad.layout :as layout]
+            [reagent.core :as r]))
+
 
 (defn example []
-  (acu/demo "Markdown-Block"
-    "Use Markdown blocks for [Markdown](https://en.wikipedia.org/wiki/Markdown) content."
-    [layout/frame]
+  (fn []
+    (acu/demo "Markdown-Block"
+      "Use Markdown blocks for [Markdown](https://en.wikipedia.org/wiki/Markdown) content."
+      [layout/frame
+       [layout/markdown-block "## here is some markdown
 
-    '[layout/frame]))
+  > a note/quote
+
+  and a hyperlink: [ring](https://github.com/ring-clojure/ring)"]]
+
+      '[layout/frame
+        [layout/markdown-block "## here is some markdown
+
+  > a note/quote
+
+  and a hyperlink: [ring](https://github.com/ring-clojure/ring)"]])))
