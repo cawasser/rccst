43865e69865cb26f36f2c5f656e0f60285e6d8ca
Neel Dave <neelkdave@yahoo.com>
2022-01-14 10:44:41 -0500

    -added radio-button for re-com

--- src/cljs/bh/rccst/views/catalog/re_com.cljs
+++ src/cljs/bh/rccst/views/catalog/re_com.cljs
@@ -1,7 +1,8 @@
 (ns bh.rccst.views.catalog.re-com
   (:require [bh.rccst.views.catalog.example.table :as table]
             [bh.rccst.views.catalog.example.checkbox-re-com :as checkbox]
-            [bh.rccst.views.catalog.example.progress-bar :as progress-bar]))
+            [bh.rccst.views.catalog.example.progress-bar :as progress-bar]
+            [bh.rccst.views.catalog.example.radio-button :as radio-button]))
 
 
 
@@ -12,4 +13,6 @@
 
    [checkbox/example]
 
-   [progress-bar/example]])
+   [progress-bar/example]
+
+   [radio-button/example]])
