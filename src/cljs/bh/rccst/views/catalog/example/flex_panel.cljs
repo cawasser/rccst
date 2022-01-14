1306a16063ee3ec8f52168f9e87e16c218251c51
Chris Wasser <cawasser@mac.com>
2022-01-14 12:39:10 -0500

    - markdown updates for container demos

--- src/cljs/bh/rccst/views/catalog/example/flex_panel.cljs
+++ src/cljs/bh/rccst/views/catalog/example/flex_panel.cljs
@@ -4,20 +4,20 @@
             [woolybear.packs.flex-panel :as flex]))
 
 (defn example []
-      (acu/demo "Flex panel"
-                "A flex panel is designed primarily to implement a container that fills the
-                entire viewport, with a scrolling content section between an (optional) flex-top
-                and/or flex-bottom component. Specify the :height as 100vh to fill the entire
-                screen, or use any valid CSS height specification to set a different size."
-                [flex/flex-panel {:height "33vh"}
-                 [flex/flex-top "This line should be fixed to the top of the flex panel."]
-                 [flex/flex-bottom "This line should be fixed to the bottom of the flex panel."]
-                 [layout/text-block acu/lorem]
-                 [layout/text-block acu/lorem]
-                 [layout/text-block acu/lorem]]
-                '[flex/flex-panel {:height "33vh"}
-                  [flex/flex-top "This line should be fixed to the top of the flex panel."]
-                  [flex/flex-bottom "This line should be fixed to the bottom of the flex panel."]
-                  [layout/text-block acu/lorem]
-                  [layout/text-block acu/lorem]
-                  [layout/text-block acu/lorem]]))
+  (acu/demo "Flex panel"
+    "A flex panel is designed primarily to implement a container that fills the
+    entire viewport, with a scrolling content section between an (optional) flex-top
+    and/or flex-bottom component. Specify the `:height` as `\"100vh\"` to fill the entire
+    screen, or use any valid CSS height specification to set a different size."
+    [flex/flex-panel {:height "33vh"}
+     [flex/flex-top "This line should be fixed to the top of the flex panel."]
+     [flex/flex-bottom "This line should be fixed to the bottom of the flex panel."]
+     [layout/text-block acu/lorem]
+     [layout/text-block acu/lorem]
+     [layout/text-block acu/lorem]]
+    '[flex/flex-panel {:height "33vh"}
+      [flex/flex-top "This line should be fixed to the top of the flex panel."]
+      [flex/flex-bottom "This line should be fixed to the bottom of the flex panel."]
+      [layout/text-block acu/lorem]
+      [layout/text-block acu/lorem]
+      [layout/text-block acu/lorem]]))
--- src/cljs/bh/rccst/views/catalog/example/shy_block.cljs
+++ src/cljs/bh/rccst/views/catalog/example/shy_block.cljs
@@ -6,29 +6,29 @@
             [woolybear.ad.buttons :as buttons]))
 
 (defn shy-block-demo
-      []
-      (let [shy-block-active? (ratom/atom true)
-            click-dispatcher (fn [_]
-                                 (swap! shy-block-active? not))]
-           (fn []
-               (let [active? @shy-block-active?]
-                    [layout/frame
-                     [layout/columns
-                      [layout/column {:extra-classes :is-one-fifth}
-                       [layout/padded
-                        [buttons/button {:on-click click-dispatcher} "Toggle Visibility"]]]
-                      [:div.column
-                       [containers/shy-block {:active? active?}
-                        [layout/text-block acu/lorem]]]]]))))
+  []
+  (let [shy-block-active? (ratom/atom true)
+        click-dispatcher (fn [_]
+                           (swap! shy-block-active? not))]
+    (fn []
+      (let [active? @shy-block-active?]
+        [layout/frame
+         [layout/columns
+          [layout/column {:extra-classes :is-one-fifth}
+           [layout/padded
+            [buttons/button {:on-click click-dispatcher} "Toggle Visibility"]]]
+          [:div.column
+           [containers/shy-block {:active? active?}
+            [layout/text-block acu/lorem]]]]]))))
 
 (defn example []
-      (acu/demo "Shy block"
-                "A 'shy' block is a container that is only visible when its :active? option
-                is true. Used as a sub-component of other components such as the 'spoiler'
-                panel. Takes standard :extra-classes and :subscribe-to-classes options."
-                [shy-block-demo]
+  (acu/demo "Shy block"
+    "A 'shy' block is a container that is only visible when its `:active?` option
+    is true. Used as a sub-component of other components such as the 'spoiler'
+    panel. Takes standard `:extra-classes` and `:subscribe-to-classes` options."
+    [shy-block-demo]
 
-                '[layout/frame
-                  [buttons/button {:on-click click-dispatcher} "Toggle Visibility"]
-                  [containers/shy-block {:active? active?}
-                   [layout/text-block acu/lorem]]]))
+    '[layout/frame
+      [buttons/button {:on-click click-dispatcher} "Toggle Visibility"]
+      [containers/shy-block {:active? active?}
+       [layout/text-block acu/lorem]]]))
