f2b9fd44f00b33a33c934b4ac9cc39cd2bdaa700
George Pierce <gpierce3@hotmail.com>
2022-01-17 16:47:26 -0500

    Added overviews of tech used in server.

---
+++ src/cljs/bh/rccst/views/technologies/overview/jackdaw.cljs
@@ -0,0 +1,9 @@
+(ns bh.rccst.views.technologies.overview.jackdaw
+  (:require [bh.rccst.views.technologies.overview.overview :as o]))
+
+(defn overview []
+      [o/overview
+       "Compojure"
+       "Discuss [Compojure](https://github.com/weavejester/compojure) here."])
+
+
---
+++ src/cljs/bh/rccst/views/technologies/overview/transit.cljs
@@ -0,0 +1,8 @@
+(ns bh.rccst.views.technologies.overview.transit
+  (:require [bh.rccst.views.technologies.overview.overview :as o]))
+
+(defn overview []
+      [o/overview
+       "Compojure"
+       "Discuss [Compojure](https://github.com/weavejester/compojure) here."])
+
