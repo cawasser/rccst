2d06aaa28d98c5672cfad1e266a35dc3876e7c7c
Chris Wasser <cawasser@mac.com>
2022-01-18 12:37:31 -0500

    - added navbar example

--- src/cljs/bh/rccst/db.cljs
+++ src/cljs/bh/rccst/db.cljs
@@ -1,15 +1,18 @@
 (ns bh.rccst.db
   (:require [bh.rccst.ui-component.navbar :as nav-bar]
             [bh.rccst.views.catalog :as catalog]
-            [bh.rccst.views.technologies :as tech]))
+            [bh.rccst.views.technologies :as tech]
+
+            [bh.rccst.views.catalog.example.navbar :as navbar]))
 
 
 (def default-db
-  {:name       "RCCST"
-   :version    ""
-   :logged-in? false
-   :sources    {:number 0
-                :string "empty"}
-   :nav-bar    nav-bar/init-db
-   :catalog    catalog/init-db
-   :tech       tech/init-db})
+  {:name           "RCCST"
+   :version        ""
+   :logged-in?     false
+   :sources        {:number 0
+                    :string "empty"}
+   :nav-bar        nav-bar/init-db
+   :catalog        catalog/init-db
+   :tech           tech/init-db
+   :navbar-example navbar/init-db})
--- src/cljs/bh/rccst/views/catalog/containers.cljs
+++ src/cljs/bh/rccst/views/catalog/containers.cljs
@@ -2,7 +2,8 @@
   "Catalog and demonstrations of available container components."
   (:require [bh.rccst.views.catalog.example.v-scroll-panel :as v-scroll-panel]
             [bh.rccst.views.catalog.example.shy-block :as shy-block]
-            [bh.rccst.views.catalog.example.flex-panel :as flex-panel]))
+            [bh.rccst.views.catalog.example.flex-panel :as flex-panel]
+            [bh.rccst.views.catalog.example.navbar :as navbar]))
 
 
 ; TODO: do we need this in an RCCST namespace, or can we just grab it from woolybear/catalog?
@@ -17,5 +18,7 @@
 
    [shy-block/example]
 
-   [flex-panel/example]])
+   [flex-panel/example]
+
+   [navbar/example]])
 
