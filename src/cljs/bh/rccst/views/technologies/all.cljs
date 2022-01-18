427129a69a2fcc61008df89188f851ece40205f9
Chris Wasser <cawasser@mac.com>
2022-01-18 14:27:21 -0500

    - updated for all (current) technologies on the all page

--- src/cljs/bh/rccst/views/technologies/all.cljs
+++ src/cljs/bh/rccst/views/technologies/all.cljs
@@ -2,7 +2,22 @@
   (:require [woolybear.ad.layout :as layout]
             [woolybear.packs.flex-panel :as flex]
 
-            [bh.rccst.views.technologies.overview.ring :as ring]))
+            [bh.rccst.views.technologies.overview.ring :as ring-overview]
+            [bh.rccst.views.technologies.overview.compojure :as compojure-overview]
+            [bh.rccst.views.technologies.overview.component :as component-overview]
+            [bh.rccst.views.technologies.overview.sente :as sente-overview]
+            [bh.rccst.views.technologies.overview.transit :as transit-overview]
+            [bh.rccst.views.technologies.overview.next-jdbc :as next-jdbc-overview]
+            [bh.rccst.views.technologies.overview.jackdaw :as jackdaw-overview]
+            [bh.rccst.views.technologies.overview.reagent :as reagent]
+            [bh.rccst.views.technologies.overview.re-frame :as re-frame]
+            [bh.rccst.views.technologies.overview.re-com :as re-com]
+            [bh.rccst.views.technologies.overview.woolybear :as woolybear]
+            [bh.rccst.views.technologies.overview.recharts :as recharts]
+            [bh.rccst.views.technologies.overview.kafka :as kafka]
+            [bh.rccst.views.technologies.overview.swagger :as swagger]))
+
+
 
 
 (defn page []
@@ -14,5 +29,20 @@
       [:h2.has-text-info "All"]
       [layout/text-block "An overview of many of the technologies we use in RCCST"]]]
 
-    [layout/section
-     [ring/overview]]]])
+    [ring-overview/overview]
+    [compojure-overview/overview]
+    [component-overview/overview]
+    [sente-overview/overview]
+    [transit-overview/overview]
+    [next-jdbc-overview/overview]
+    [jackdaw-overview/overview]
+
+    [reagent/overview]
+    [re-frame/overview]
+    [re-com/overview]
+    [woolybear/overview]
+    [recharts/overview]
+
+    [kafka/overview]
+    [swagger/overview]]])
+
