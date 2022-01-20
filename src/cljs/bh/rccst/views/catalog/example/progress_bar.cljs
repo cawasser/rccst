a56f9ea6b438f0ec8f7bd36c3d38c3c64f40b724
austinpaine <austin.paine@ngc.com>
2022-01-19 19:14:47 -0700

    h-boxed date-picker,input time, progress bar, and slider. added sample code

--- src/cljs/bh/rccst/views/catalog/example/progress_bar.cljs
+++ src/cljs/bh/rccst/views/catalog/example/progress_bar.cljs
@@ -11,32 +11,36 @@
       (acu/demo "Progress Bar"
                 [core/v-box :src (core/at)
                  :gap "20px"
-                 :children [[pb/progress-bar :src (core/at)
-                             :model progress
-                             :width "350px"
-                             :striped? @striped?]
-                            [core/v-box :src (core/at)
-                             :gap "10px"
-                             :style {:min-width        "150px"
-                                     :padding          "15px"
-                                     :border-top       "1px solid #DDD"
-                                     :background-color "#f7f7f7"}
-                             :children [
-                                        [core/title :src (core/at) :level :level3 :label "Interactive Parameters" :style {:margin-top "0"}]
-                                        [core/h-box :src (core/at)
-                                         :gap "10px"
-                                         :children [[core/box :src (core/at) :align :start :child [:code ":model"]]
-                                                    [core/slider :src (core/at)
+                 :children [[core/h-box :src (core/at)
+                             :justify :between
+                             :children [[core/v-box :src (core/at)
+                                         :align-self :center
+                                         :children [[pb/progress-bar :src (core/at)
                                                      :model progress
-                                                     :min 0
-                                                     :max 100
-                                                     :width "200px"
-                                                     :on-change #(reset! progress %)]
-                                                    [core/label :src (core/at) :label @progress]]]
-                                        [core/checkbox :src (core/at)
-                                         :label [core/box :src (core/at) :align :start :child [:code ":striped?"]]
-                                         :model striped?
-                                         :on-change #(reset! striped? %)]]]]]
+                                                     :width "400px"
+                                                     :striped? @striped?]]]
+                                        [core/v-box :src (core/at)
+                                         :gap "10px"
+                                         :style {:min-width        "200px"
+                                                 :padding          "15px"
+                                                 :border-top       "1px solid #DDD"
+                                                 :background-color "#f7f7f7"}
+                                         :children [
+                                                    [core/title :src (core/at) :level :level3 :label "Interactive Parameters" :style {:margin-top "0"}]
+                                                    [core/h-box :src (core/at)
+                                                     :gap "10px"
+                                                     :children [[core/box :src (core/at) :align :start :child [:code ":model"]]
+                                                                [core/slider :src (core/at)
+                                                                 :model progress
+                                                                 :min 0
+                                                                 :max 100
+                                                                 :width "200px"
+                                                                 :on-change #(reset! progress %)]
+                                                                [core/label :src (core/at) :label @progress]]]
+                                                    [core/checkbox :src (core/at)
+                                                     :label [core/box :src (core/at) :align :start :child [:code ":striped?"]]
+                                                     :model striped?
+                                                     :on-change #(reset! striped? %)]]]]]]]
 
                 '[core/v-box :src (core/at)
                   :gap "20px"
