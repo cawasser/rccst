e0f32ab0b46a373fccbb0bc40a9b899f807a8a5c
austinpaine <austin.paine@ngc.com>
2022-01-24 16:25:59 -0700

    scatter chart basis

--- src/cljs/bh/rccst/views/catalog/charts.cljs
+++ src/cljs/bh/rccst/views/catalog/charts.cljs
@@ -2,7 +2,8 @@
   (:require [bh.rccst.views.catalog.example.bar-chart :as bar-chart]
             [bh.rccst.views.catalog.example.colored-pie-chart :as colored-pie-chart]
             [bh.rccst.views.catalog.example.line-chart :as line-chart]
-            [bh.rccst.views.catalog.example.pie-chart :as pie-chart]))
+            [bh.rccst.views.catalog.example.pie-chart :as pie-chart]
+            [bh.rccst.views.catalog.example.scatter-chart :as scatter-chart]))
 
 
 
@@ -11,6 +12,7 @@
    [bar-chart/example]
    [line-chart/example]
    [pie-chart/example]
-   [colored-pie-chart/example]])
+   [colored-pie-chart/example]
+   [scatter-chart/example]])
 
 
