365f7cec058cbe38deebb243b09ff154a5fec79f
austinpaine <apaine13@gmail.com>
2022-02-10 13:07:08 -0700

    globe fengshui and funnel cleanup

--- src/cljs/bh/rccst/views/catalog/example/funnel_chart.cljs
+++ src/cljs/bh/rccst/views/catalog/example/funnel_chart.cljs
@@ -19,24 +19,3 @@
             :config-panel chart/config-panel
             :component-panel chart/component
             :source-code "temp"]))
-
-
-;(defn example []
-;      (utils/init-config-panel "funnel-chart-demo")
-;
-;      (let [data (r/atom utils/paired-data)]
-;           (bcu/configurable-demo
-;             "Funnel Chart"
-;             "Funnel Chart with different colors for each chunk.  This requires embedding `Cell` elements inside the `Funnel` element"
-;             [:funnel-chart-demo/config :funnel-chart-demo/data :funnel-chart-demo/tab-panel :funnel-chart-demo/value]
-;             [utils/tabular-data-panel data]
-;             [config-panel data config]
-;             [component data config]
-;             '[:> FunnelChart {:height 400 :width 500}
-;               (utils/non-gridded-chart-components config)
-;               [:> Funnel {:dataKey :value :data @data :isAnimationActive @isAnimationActive?}
-;                (doall
-;                  (map-indexed (fn [idx {name :name}]
-;                                   [:> Cell {:key (str "cell-" idx) :fill (get-in @config [:colors name])}])
-;                               @data))
-;                [:> LabelList {:position :right :fill "#000000" :stroke "none" :dataKey :name}]]])))
