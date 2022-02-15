6e802a3eddebef42f3db3f984e7de785b7fd236b
George Pierce <gpierce3@hotmail.com>
2022-02-15 14:35:28 -0500

    - Place holder code for Radial Bar Chart in place.

---
+++ src/cljs/bh/rccst/views/catalog/example/radial_bar_chart.cljs
@@ -0,0 +1,17 @@
+(ns bh.rccst.views.catalog.example.radial-bar-chart
+  (:require [taoensso.timbre :as log]
+
+            [bh.rccst.ui-component.atom.bar-chart :as chart]
+            [bh.rccst.ui-component.molecule.example :as example]))
+
+
+(defn example []
+      (let [widget-id "radial-bar-chart-demo"]
+           [example/component-example
+            :title "Radial Bar Chart"
+            :widget-id widget-id
+            :description "A simple Radial Bar Chart built using [Recharts]()"
+            :data chart/sample-data
+            :component chart/component
+            :component-id "radial-bar-chart-demo/radial-bar-chart"
+            :source-code chart/source-code]))
