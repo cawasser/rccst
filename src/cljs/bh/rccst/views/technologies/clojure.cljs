fcd0aa83962c3c566f160bbeae6dfbf8300a220e
Chris Wasser <cawasser@mac.com>
2022-02-22 22:51:05 -0500

    - added hiccup and more details on Clojure and ClojureScript

--- src/cljs/bh/rccst/views/technologies/clojure.cljs
+++ src/cljs/bh/rccst/views/technologies/clojure.cljs
@@ -18,7 +18,27 @@
     [flex/flex-top
      [:div
       [:h2.has-text-info "Clojure"]
-      [layout/markdown-block "Discuss [Clojure](https://clojure.org) here"]
+      [layout/markdown-block
+       "[Clojure](https://clojure.org) is a dynamic, general-purpose programming language, combining the approachability and
+       interactive development of a scripting language with an efficient and robust infrastructure for multithreaded programming.
+       [Clojure](https://clojure.org) is a compiled language, yet remains completely dynamic – every feature supported by
+       [Clojure](https://clojure.org) is supported at runtime. [Clojure](https://clojure.org) provides easy access to the Java
+       frameworks, with optional type hints and type inference, to ensure that calls to Java can avoid reflection.
+
+[Clojure](https://clojure.org) is a dialect of [Lisp](https://en.wikipedia.org/wiki/Lisp_(programming_language)), and shares with
+Lisp the code-as-data philosophy and a powerful macro system.
+[Clojure](https://clojure.org) is predominantly a functional programming language, and features a rich set of immutable,
+persistent data structures. When mutable state is needed, Clojure offers a software transactional memory system and reactive
+Agent system that ensure clean, correct, multithreaded designs.
+
+> See also:
+>
+>[Clojure](https://clojure.org)
+>
+>[Lisp](https://en.wikipedia.org/wiki/Lisp_(programming_language))
+>
+>[Functional Programming ](https://en.wikipedia.org/wiki/Functional_programming)"]
+
       [layout/section "We use a number of libraries to provide critical server-side functionality:"]]]
 
     [ring-overview/overview]
--- src/cljs/bh/rccst/views/technologies/clojurescript.cljs
+++ src/cljs/bh/rccst/views/technologies/clojurescript.cljs
@@ -1,6 +1,7 @@
 (ns bh.rccst.views.technologies.clojurescript
   (:require [woolybear.ad.layout :as layout]
             [woolybear.packs.flex-panel :as flex]
+            [bh.rccst.views.technologies.overview.hiccup :as hiccup]
             [bh.rccst.views.technologies.overview.reagent :as reagent]
             [bh.rccst.views.technologies.overview.re-frame :as re-frame]
             [bh.rccst.views.technologies.overview.re-com :as re-com]
@@ -20,10 +21,16 @@
       targets JavaScript. It emits JavaScript code which is compatible with the advanced compilation mode of the Google Closure
       optimizing compiler to provide the most powerful language for programming the web. [Clojurescript](https://clojurescript.org/)
       is a robust, practical, and fast programming language with a set of useful features that together form a simple, coherent,
-      and powerful tool."]
+      and powerful tool.
+
+> See also:
+>
+>[Clojurescript](https://clojurescript.org/)"]
       [layout/section "We use a number of libraries to provide critical client-side functionality:"]]]
     [layout/frame
      [reagent/overview]]
+    [layout/frame
+     [hiccup/overview]]
     [layout/frame
      [re-frame/overview]]
     [layout/frame
