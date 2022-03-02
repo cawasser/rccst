255cbac5a2900d09844bef994f4cb7103d0955ad
Neel <neelkdave@yahoo.com>
2022-03-02 09:38:40 -0500

    Created globe using resium

---
+++ src/cljs/bh/rccst/ui_component/atom/resium/globe.cljs
@@ -0,0 +1,13 @@
+(ns bh.rccst.ui-component.atom.resium.globe
+  (:require ["resium" :refer (Viewer Entity)]
+            ["cesium" :refer (Cartesian3)]
+            [taoensso.timbre :as log]))
+
+(defn globe []
+      (log/info "resium Globe")
+      [:div
+       [:> Viewer
+        [:> Entity {:description "test"
+                    :name "tokyo"
+                    :point {:pixelSize 10}
+                    :position (.fromDegrees Cartesian3 139.7 35.7 100)}]]])
