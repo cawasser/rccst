b7b759441baa03811c0414a18a5185a9d92870b9
Chris Wasser <cawasser@mac.com>
2022-03-03 15:22:29 -0500

    Revert "Merge branch 'master' of https://github.com/cawasser/rccst"
    This reverts commit 399ec4a2e39eb319ff046454ffdbe5657d5d8771, reversing
    changes made to 33adec380f2b04db2111a95485a92ca12f1fe889.

--- src/cljs/bh/rccst/views/atom/miscellaneous.cljs
+++ src/cljs/bh/rccst/views/atom/miscellaneous.cljs
@@ -1,7 +1,6 @@
 (ns bh.rccst.views.atom.miscellaneous
   "Catalog and demonstrations of miscellaneous components that don't fit anywhere else."
-  (:require [bh.rccst.views.atom.example.misc.resium.globe :as globe]
-            [bh.rccst.views.atom.example.misc.hex-color-picker :as hex-picker]
+  (:require [bh.rccst.views.atom.example.misc.hex-color-picker :as hex-picker]
             [bh.rccst.views.atom.example.misc.rgba-color-picker :as rgba-picker]
             [bh.rccst.views.atom.example.misc.two-d-globe :as two-d-globe]
             [bh.rccst.views.atom.example.misc.three-d-globe :as three-d-globe]))
@@ -11,7 +10,6 @@
   []
 
   [:div
-   [globe/example]
    [hex-picker/example]
    [rgba-picker/example]
    [two-d-globe/example]
