0b0c4bd0a058f802c4dbeb0321897d72a7a8c54b
Neel Dave <neelkdave@yahoo.com>
2022-03-08 11:24:58 -0500

    Added defaultAccessToken for Cesium Ion

--- src/cljs/bh/rccst/ui_component/atom/resium/globe.cljs
+++ src/cljs/bh/rccst/ui_component/atom/resium/globe.cljs
@@ -1,13 +1,14 @@
 (ns bh.rccst.ui-component.atom.resium.globe
-  (:require ["resium" :refer (Viewer Entity)]
-            ["cesium" :refer (Cartesian3)]
+  (:require ["resium" :refer (Viewer Globe Entity)]
+            ["cesium" :refer (Cartesian3 Ion)]
             [taoensso.timbre :as log]))
 
 (defn globe []
       (log/info "resium Globe")
+      (set! (.-defaultAccessToken Ion) "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWNiMDFiNy1iYzFiLTQ2NDMtYmJlNC0zMjRiNTIzMjM5ODQiLCJpZCI6ODQ1MDAsImlhdCI6MTY0NjMyODY1Mn0.Nax1YEWqQzM_eOqHPhblhU9TO9U42VJn4wCcolAkuhM")
       [:div#resium-globe
        [:> Viewer
-        [:> Entity {:description "test"
-                    :name "tokyo"
-                    :point {:pixelSize 10}
-                    :position (.fromDegrees Cartesian3 139.7 35.7 100)}]]])
+        [:> Globe [:> Entity {:description "test"
+                              :name "tokyo"
+                              :point {:pixelSize 10}
+                              :position (.fromDegrees Cartesian3 139.7 35.7 100)}]] ]])
