9467308d1c819527b8adc2d807560f9b88c50d9b
Neel Dave <neelkdave@yahoo.com>
2022-03-11 15:10:00 -0500

    T233 resium (#4)
    * Created globe using resium
    
    * Added widgets.css file
    
    * Added defaultAccessToken for Cesium Ion
    
    * Copied Cesium folder from node_module to resources
    
    * Copied Cesium folder from node_module to resources
    
    * Added PolygonGraphics
    
    * Added PolylineGraphics

---
+++ src/cljs/bh/rccst/views/atom/example/misc/resium/globe.cljs
@@ -0,0 +1,7 @@
+(ns bh.rccst.views.atom.example.misc.resium.globe
+  (:require [bh.rccst.ui-component.atom.resium.globe :as globe]
+            [woolybear.ad.catalog.utils :as acu]))
+
+(defn example []
+      (acu/demo "Resium Globe"
+                [globe/globe]))
--- src/cljs/bh/rccst/views/atom/miscellaneous.cljs
+++ src/cljs/bh/rccst/views/atom/miscellaneous.cljs
@@ -1,6 +1,7 @@
 (ns bh.rccst.views.atom.miscellaneous
   "Catalog and demonstrations of miscellaneous components that don't fit anywhere else."
-  (:require [bh.rccst.views.atom.example.misc.hex-color-picker :as hex-picker]
+  (:require [bh.rccst.views.atom.example.misc.resium.globe :as globe]
+            [bh.rccst.views.atom.example.misc.hex-color-picker :as hex-picker]
             [bh.rccst.views.atom.example.misc.rgba-color-picker :as rgba-picker]
             [bh.rccst.views.atom.example.misc.two-d-globe :as two-d-globe]
             [bh.rccst.views.atom.example.misc.three-d-globe :as three-d-globe]))
@@ -13,4 +14,5 @@
    [hex-picker/example]
    [rgba-picker/example]
    [two-d-globe/example]
-   [three-d-globe/example]])
+   [three-d-globe/example]
+   [globe/example]])
