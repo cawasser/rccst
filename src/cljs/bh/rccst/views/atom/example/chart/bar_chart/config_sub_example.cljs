eb278fa27022f5f150295f36b8212d4bd58dfcea
Chris Wasser <cawasser@mac.com>
2022-04-25 14:31:43 -0400

    - config-sub-example now features tools, so the user can adjust various configuration properties of the chart via event/subscription

--- src/cljs/bh/rccst/views/atom/example/chart/bar_chart/config_sub_example.cljs
+++ src/cljs/bh/rccst/views/atom/example/chart/bar_chart/config_sub_example.cljs
@@ -5,6 +5,7 @@
             [bh.rccst.ui-component.utils :as ui-utils]
             [bh.rccst.ui-component.utils.helpers :as h]
             [reagent.core :as r]
+            [re-com.core :as rc]
             [re-frame.core :as re-frame]
             [taoensso.timbre :as log]))
 
@@ -24,6 +25,70 @@
    :container  ""})
 
 
+(defn- show-config [config-data]
+  [rc/h-box :src (rc/at)
+   :gap "10px"
+   :style {:border     "1px solid" :border-radius "3px"
+           :background "#808080"
+           :box-shadow "5px 5px 5px 2px"
+           :margin     "5px" :padding "5px"}
+   :children [[:p {:style {:color "white"}}
+               (str @config-data)]]])
+
+
+(defn- config-tools [config-data]
+  (let [brush? (ui-utils/subscribe-local config-data [:brush])
+        uv? (ui-utils/subscribe-local config-data [:uv :include])
+        tv? (ui-utils/subscribe-local config-data [:tv :include])]
+
+    (fn []
+      [rc/h-box :src (rc/at)
+       :gap "10px"
+       :style {:border     "1px solid" :border-radius "3px"
+               :box-shadow "5px 5px 5px 2px"
+               :margin     "5px" :padding "5px"}
+       :children [[rc/button :on-click #() :label "Default"]
+                  [rc/button :on-click #(h/handle-change-path config-data [:brush] (not @brush?))
+                   :label "!Brush"]
+                  [rc/button :on-click #(h/handle-change-path config-data [:uv :include] (not @uv?))
+                   :label "! uv data"]
+                  [rc/button :on-click #(h/handle-change-path config-data [:tv :include] (not @tv?))
+                   :label "! tv data"]
+                  [chart-utils/color-config config-data ":amt :fill" [:amt :fill] :above-center]
+                  [rc/button :on-click #((h/handle-change-path config-data [:uv :stackId] "b")
+                                         (h/handle-change-path config-data [:pv :stackId] "b"))
+                   :label "stack uv/pv"]
+                  [rc/button :on-click #((h/handle-change-path config-data [:uv :stackId] "")
+                                         (h/handle-change-path config-data [:pv :stackId] ""))
+                   :label "!stack uv/pv"]
+                  [rc/button :on-click #((h/handle-change-path config-data [:tv :stackId] "a")
+                                         (h/handle-change-path config-data [:amt :stackId] "a"))
+                   :label "stack tv/amt"]
+                  [rc/button :on-click #((h/handle-change-path config-data [:tv :stackId] "")
+                                         (h/handle-change-path config-data [:amt :stackId] ""))
+                   :label "!stack tv/amt"]]])))
+
+
+(defn- config-update-example [& {:keys [data config-data container-id component-id] :as params}]
+  ;(log/info "config-update-example (params)" params)
+
+  [rc/v-box :src (rc/at)
+   :gap "10px"
+   :width "100%"
+   :height "100%"
+   :children [[:div.chart-part {:style {:width "100%" :height "90%"}}
+               [chart/component
+                :data data
+                :config-data config-data
+                :component-id component-id
+                :container-id container-id
+                :component-panel chart/component]]
+
+              [show-config (h/resolve-value config-data)]
+
+              [:div.config-tools-part {:style {:width "100%"}}
+               [config-tools config-data]]]])
+
 (defn- component [& {:keys [data config-data component-id container-id] :as params}]
   (let [id (r/atom nil)]
 
@@ -33,7 +98,7 @@
         (ui-utils/init-widget @id (config default-config-data))
         (ui-utils/dispatch-local @id [:container] container-id))
 
-      [chart/component
+      [config-update-example
        :data data
        :config-data config-data
        :component-id (h/path->keyword component-id "bar-chart-2")
@@ -45,7 +110,7 @@
         component-id (h/path->keyword container-id "bar-chart-2")]
 
     [e/component-example
-     :title "Bar Chart 2 (Live Configuration - subscription) (WIP)"
+     :title "Bar Chart 2 (Live Configuration - subscription)"
      :description "A Bar Chart (2) built using [Recharts](https://recharts.org/en-US/api/BarChart). This example shows how
      charts can take [subscriptions](https://day8.github.io/re-frame/subscriptions/) as input and re-render as the configuration changes.
 
