8d29f41c0fc62dd9a6332a6731616f737dabf7b3
Neel Dave <neelkdave@yahoo.com>
2022-04-27 12:46:29 -0400

    Realized I had a duplicate row1 in data; removed it

--- src/cljs/bh/rccst/views/atom/example/astrouxds/table.cljs
+++ src/cljs/bh/rccst/views/atom/example/astrouxds/table.cljs
@@ -6,11 +6,10 @@
 (defn example []
   (acu/demo
     "Table"
-    "First row is duplicated and takes away spot for last row"
     [rc/h-box :src (rc/at)
      :gap "50px"
      :children [[table/table ["name" "kp" "uv" "pv" "amt"]
-                             ["Page A" "2000" "4000" "2400" "2400"]                             ["Page A" "2000" "4000" "2400" "2400"]
+                             ["Page A" "2000" "4000" "2400" "2400"]
                              ["Page B" "2000" "3000" "5598" "2210"]
                              ["Page C" "2000" "2000" "9800" "2290"]
                              ["Page D" "2000" "2780" "3908" "2000"]
