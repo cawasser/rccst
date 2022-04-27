1775f32f0bd0e75a50c9d5abf09ffa20b385fed3
Neel Dave <neelkdave@yahoo.com>
2022-04-27 10:17:17 -0400

    Moved the data to views side; table component in WIP

--- src/cljs/bh/rccst/ui_component/atom/astrouxds/table.cljs
+++ src/cljs/bh/rccst/ui_component/atom/astrouxds/table.cljs
@@ -1,26 +1,15 @@
 (ns bh.rccst.ui-component.atom.astrouxds.table
   (:require ["@astrouxds/react" :refer (RuxTable RuxTableHeader RuxTableHeaderRow RuxTableHeaderCell RuxTableBody RuxTableRow RuxTableCell)]))
 
-(defn table []
+(defn table [headerData row1 row2 row3 row4 row5]
    [:> RuxTable
     [:> RuxTableHeader
      [:> RuxTableHeaderRow
-       (doall (for [v ["name" "kp" "uv" "pv" "amt"]]
-                [:> RuxTableHeaderCell v]))]]
+       (doall (for [h headerData]
+                [:> RuxTableHeaderCell h]))]]
     [:> RuxTableBody
-     [:> RuxTableRow {:selected false}
-      (doall (for [v ["Page A" "2000" "4000" "2400" "2400"]]
-               [:> RuxTableCell v]))]
-     [:> RuxTableRow {:selected false}
-      (doall (for [v ["Page B" "2000" "3000" "5598" "2210"]]
-               [:> RuxTableCell v]))]
-     [:> RuxTableRow {:selected false}
-      (doall (for [v ["Page C" "2000" "2000" "9800" "2290"]]
-               [:> RuxTableCell v]))]
-     [:> RuxTableRow {:selected false}
-      (doall (for [v ["Page D" "2000" "2780" "3908" "2000"]]
-               [:> RuxTableCell v]))]
-     [:> RuxTableRow {:selected false}
-      (doall (for [v ["Page E" "2000" "1890" "4800" "2181"]]
-               [:> RuxTableCell v]))]]])
+     (doall (for [r [row1 row2 row3 row4 row5]]
+              [:> RuxTableRow {:selected false}
+               (doall (for [c r]
+                        [:> RuxTableCell c]))]))]])
 
