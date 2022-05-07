71eb4652e1a60fb68b8580acecd69e8b2fd6c280
Chris Wasser <cawasser@mac.com>
2022-05-07 17:23:58 -0400

    - typography change for clojurescript libraries

--- src/cljs/bh/rccst/views/technologies/overview/re_com.cljs
+++ src/cljs/bh/rccst/views/technologies/overview/re_com.cljs
@@ -4,18 +4,19 @@
 (defn overview []
       (o/overview "Re-com"
 
-"#### A ClojureScript library of UI components for Reagent.\n
-**re-com provides:**\n\n
-- familiar UI widgetry components such as dropdowns, date pickers, popovers, tabs, etc.\n
-- layout components, which arrange widgets vertically and horizontally, within splitters, etc. Plus components which put borders
-around their children. These various pieces can be arbitrarily nested to create sophisticated layouts.\n
-- a mostly Bootstrap look, mixed with some Material Design Icons.\n
-- UI basics to build a desktop-class SPA app.\n
+"#### _A ClojureScript library of UI components for Reagent_
+
+**re-com provides:**
+- Familiar UI widgets/components such as dropdowns, date pickers, popovers, tabs, etc.
+- Layout components, which arrange widgets vertically and horizontally, within splitters, etc. Plus components which put borders
+around their children. These various pieces can be arbitrarily nested to create sophisticated layouts.
+- A mostly Bootstrap look, mixed with some Material Design Icons.
+- UI basics to build a desktop-class SPA app.
 - Use of the [Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/). The entire layout side of this library
-relies on Flexbox.\n\n
+relies on Flexbox.
 
 See also:
->[re-com/project](https://github.com/day8/re-com)\n
+>[re-com/project](https://github.com/day8/re-com)
 >[re-com/demo](https://re-com.day8.com.au/)"
 
 "/imgs/re-com-logo.png"))
--- src/cljs/bh/rccst/views/technologies/overview/re_frame.cljs
+++ src/cljs/bh/rccst/views/technologies/overview/re_frame.cljs
@@ -4,17 +4,18 @@
 (defn overview []
       (o/overview "Re-frame"
 
-"#### A framework for building Modern Web Apps in ClojureScript.\n
+"#### _A framework for building Modern Web Apps in ClojureScript_
+
 re-frame is a ClojureScript framework for building user interfaces. It has a data-oriented, functional design.
 It's primary focus is on high programmer productivity and scaling up to larger Single-Page applications.
 It is about data, and the functions which transform that data. And, because it is a reactive framework,
- data coordinates functions, not the other way around.\n\n
+ data coordinates functions, not the other way around.
 re-frame puts all application state into one place, which is called *app-db*. The data in app-db can be given a
-strong schema so that, at any moment, we can validate all the data in the application.\n\n
-\n\n **Data Loop:**  re-frame implements \"a perpetual loop\". To build an app, you hang pure functions on
+strong schema so that, at any moment, we can validate all the data in the application.
+ **Data Loop:**  re-frame implements \"a perpetual loop\". To build an app, you hang pure functions on
 certain parts of this loop, and re-frame looks after the conveyance of data around the loop, into and out of the transforming
-functions you provide.\n\n Each iteration of the re-frame loop has 6 stages, and these stages happen one after another. These
-stages are: \n\n
+functions you provide. Each iteration of the re-frame loop has 6 stages, and these stages happen one after another. These
+stages are: 
 
   1. **Event dispatch:** re-frame is *event driven*. An event is sent when something happens -
   the user clicks a button, or a websocket receives a new message.
@@ -27,7 +28,7 @@ stages are: \n\n
   5. **View:** It is many *ViewFunctions* (aka Reagent components) which collectively render the UI of the application.
   Each *ViewFunction* renders part of the whole. These functions compute and return data in a format called hiccup which represents DOM.
   6. **DOM:** This is the step in which the hiccup-formatted \"descriptions of required DOM\", returned by the *ViewFunctions* of
-  previous stage, are actioned. The browser DOM nodes are mutated.\n\n
+  previous stage, are actioned. The browser DOM nodes are mutated.
 
 See also:
 >[Re-frame](https://day8.github.io/re-frame/)"
