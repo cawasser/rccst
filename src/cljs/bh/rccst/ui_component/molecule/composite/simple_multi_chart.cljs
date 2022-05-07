7a137fa12f64726373a04cd1aae87230bfa196ad
Chris Wasser <cawasser@mac.com>
2022-05-07 14:53:38 -0400

    - made layout fill (pseudo) width completely so UI Grid looks better

--- src/cljs/bh/rccst/ui_component/molecule/composite/simple_multi_chart.cljs
+++ src/cljs/bh/rccst/ui_component/molecule/composite/simple_multi_chart.cljs
@@ -19,8 +19,8 @@
                  :topic/data     {:type :source/local :name :topic/data :default sample-data}}
    :links       {:topic/data     {:data {:ui/bar-chart   :data
                                          :ui/line-chart  :data}}}
-   :grid-layout [{:i :ui/line-chart :x 0 :y 0 :w 7 :h 11 :static true}
-                 {:i :ui/bar-chart :x 7 :y 0 :w 7 :h 11 :static true}]})
+   :grid-layout [{:i :ui/line-chart :x 0 :y 0 :w 10 :h 11 :static true}
+                 {:i :ui/bar-chart :x 10 :y 0 :w 10 :h 11 :static true}]})
 
 
 (def source-code '(let [def {:components  {:ui/bar-chart   {:type :ui/component :name :rechart/bar-2}
