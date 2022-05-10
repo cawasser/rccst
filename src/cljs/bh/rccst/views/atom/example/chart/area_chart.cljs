7e25d3b14f1a5ca5a13c9bfc1e00663de22ddc05
gpierce3 <gpierce3@hotmail.com>
2022-05-10 13:19:53 -0400

    - Changing over to do examples by 'data source' and chart, rather than chart and 'data source' - WIP

--- src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
+++ src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
@@ -1,23 +1,8 @@
 (ns bh.rccst.views.atom.example.chart.area-chart
-  (:require [bh.rccst.views.atom.example.chart.area-chart.area-chart :as area-chart]
-            [bh.rccst.views.atom.example.chart.area-chart.data-ratom-example :as data-ratom]))
+  (:require [bh.rccst.ui-component.atom.chart.area-chart-2 :as chart]
+            [bh.rccst.ui-component.atom.chart.utils :as chart-utils]))
 
 (defn examples []
   [:div
-   [area-chart/example]
-   [data-ratom/example]])
+   [:p "Examples go here"]])
 
-;
-; this code was moved to an area-chart folder.
-; keeping it here until I can finish refactoring
-;
-;(defn example []
-;  (let [container-id "area-chart-demo"]
-;    [example/component-example
-;     :container-id container-id
-;     :title "Area Chart"
-;     :description "A simple Area Chart built using [Recharts](https://recharts.org/en-US/api/AreaChart)"
-;     :data chart/sample-data
-;     :component chart/configurable-component
-;     :component-id (utils/path->keyword container-id "area-chart")
-;     :source-code chart/source-code]))
