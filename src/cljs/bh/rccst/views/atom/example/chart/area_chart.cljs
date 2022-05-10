534fb75a84abc4e4c85347f78a3c5cbe1dbdf2c6
gpierce3 <gpierce3@hotmail.com>
2022-05-10 13:42:15 -0400

    - Added code for data-sub example.

--- src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
+++ src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
@@ -3,7 +3,8 @@
             [bh.rccst.ui-component.atom.chart.utils :as chart-utils]
             [bh.rccst.views.atom.example.chart.alt.data-tools :as data-tools]
             [bh.rccst.views.atom.example.chart.alt.data-ratom-example :as data-ratom-example]
-            [bh.rccst.views.atom.example.chart.alt.data-structure-example :as data-structure-example]))
+            [bh.rccst.views.atom.example.chart.alt.data-structure-example :as data-structure-example]
+            [bh.rccst.views.atom.example.chart.alt.data-sub-example :as data-sub-example]))
 
 (defn- data-ratom []
   [data-ratom-example/example
@@ -24,9 +25,9 @@
 
 (defn- data-structure []
   [data-structure-example/example
-   :container-id :bar-chart-2-data-structure-demo
-   :title "Bar Chart 2 (Live Data - structure)"
-   :description "A Bar Chart (2) built using [Recharts](https://recharts.org/en-US/api/BarChart). This example shows how
+   :container-id :area-chart-2-data-structure-demo
+   :title "Area Chart 2 (Live Data - structure)"
+   :description "An Area Chart (2) built using [Recharts](https://recharts.org/en-US/api/AreaChart). This example shows how
   charts can take [ratoms](http://reagent-project.github.io/docs/master/reagent.ratom.html) as input and re-render as the data changes.
 
   > In _this_ case, we are using a plain data structure for the data, so there is no way to update it (it lives
@@ -37,8 +38,26 @@
    :data-panel chart-utils/meta-tabular-data-panel
    :config-panel chart/config-panel])
 
+(defn data-sub []
+  [data-sub-example/example
+   :container-id :area-chart-2-data-sub-demo
+   :title "Area Chart 2 (Live Data - subscription)"
+   :description "An Area Chart (2) built using [Recharts](https://recharts.org/en-US/api/AreaChart). This example shows how
+     charts can take [subscriptions](https://day8.github.io/re-frame/subscriptions/) as input and re-render as the configuration changes.
+
+> In _this_ case, we are using a subscription to handle the data for the chart.
+"
+   :sample-data [:area-chart-2-data-sub-demo :blackboard :topic.sample-data]
+   :default-data chart/sample-data
+   :data-tools data-tools/meta-tabular-data-sub-tools
+   :source-code chart/source-code
+   :component chart/component
+   :data-panel chart-utils/meta-tabular-data-panel
+   :config-panel chart/config-panel])
+
 (defn examples []
   [:div
    [data-ratom]
-   [data-structure]])
+   [data-structure]
+   [data-sub]])
 
