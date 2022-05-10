f726d133c6687ea4e6624110136ef79e59039a4b
gpierce3 <gpierce3@hotmail.com>
2022-05-10 13:38:43 -0400

    - Added code for data-structure example. - Removed place-holder code.

--- src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
+++ src/cljs/bh/rccst/views/atom/example/chart/area_chart.cljs
@@ -2,7 +2,8 @@
   (:require [bh.rccst.ui-component.atom.chart.area-chart-2 :as chart]
             [bh.rccst.ui-component.atom.chart.utils :as chart-utils]
             [bh.rccst.views.atom.example.chart.alt.data-tools :as data-tools]
-            [bh.rccst.views.atom.example.chart.alt.data-ratom-example :as data-ratom-example]))
+            [bh.rccst.views.atom.example.chart.alt.data-ratom-example :as data-ratom-example]
+            [bh.rccst.views.atom.example.chart.alt.data-structure-example :as data-structure-example]))
 
 (defn- data-ratom []
   [data-ratom-example/example
@@ -21,8 +22,23 @@
    :data-panel chart-utils/meta-tabular-data-panel
    :config-panel chart/config-panel])
 
+(defn- data-structure []
+  [data-structure-example/example
+   :container-id :bar-chart-2-data-structure-demo
+   :title "Bar Chart 2 (Live Data - structure)"
+   :description "A Bar Chart (2) built using [Recharts](https://recharts.org/en-US/api/BarChart). This example shows how
+  charts can take [ratoms](http://reagent-project.github.io/docs/master/reagent.ratom.html) as input and re-render as the data changes.
+
+  > In _this_ case, we are using a plain data structure for the data, so there is no way to update it (it lives
+  > only inside the chart, with no way to get at it from outside)."
+   :sample-data chart/sample-data
+   :source-code chart/source-code
+   :component chart/component
+   :data-panel chart-utils/meta-tabular-data-panel
+   :config-panel chart/config-panel])
+
 (defn examples []
   [:div
-   [:p "Examples go here"]
-   [data-ratom]])
+   [data-ratom]
+   [data-structure]])
 
