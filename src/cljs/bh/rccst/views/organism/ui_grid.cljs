8e4363794fe688a29dc2ba37f255359693136813
Chris Wasser <cawasser@mac.com>
2022-05-14 17:57:19 -0400

    - refactored ui-grid from "template" to "organism" which is a better fit in AD

---
+++ src/cljs/bh/rccst/views/organism/ui_grid.cljs
@@ -0,0 +1,8 @@
+(ns bh.rccst.views.organism.ui-grid
+  (:require [bh.rccst.views.organism.ui-grid.ratom-example :as ratom-example]))
+
+
+(defn page []
+  [:div
+   [ratom-example/example]])
+
--- src/cljs/bh/rccst/views/template/ui_grid.cljs
+++
@@ -1,8 +0,0 @@
-(ns bh.rccst.views.template.ui-grid
-  (:require [bh.rccst.views.template.ui-grid.ratom-example :as ratom-example]))
-
-
-(defn page []
-  [:div
-   [ratom-example/example]])
-
