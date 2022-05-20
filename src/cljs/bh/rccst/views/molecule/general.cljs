f9d9af644c764a59ddbfd287e77c96ec225f1cc5
Chris Wasser <cawasser@mac.com>
2022-05-20 11:57:05 -0400

    - reactivated all the molecule examples

--- src/cljs/bh/rccst/views/molecule/general.cljs
+++ src/cljs/bh/rccst/views/molecule/general.cljs
@@ -15,12 +15,12 @@
   []
 
   [:div
-   ;[chart-remote-data/example]
-   ;[simple-multi-chart/example]
-   ;[simple-multi-chart-2/example]
-   ;[multi-chart-widget/example]
-   [coverage-plan/ww-example]])
-   ;[coverage-plan/r-example]
-   ;[header-bar/example]
-   ;[login/example]
-   ;[two-d-three-d-globe/example]])
+   [chart-remote-data/example]
+   [simple-multi-chart/example]
+   [simple-multi-chart-2/example]
+   [multi-chart-widget/example]
+   [coverage-plan/ww-example]
+   [coverage-plan/r-example]
+   [header-bar/example]
+   [login/example]
+   [two-d-three-d-globe/example]])
