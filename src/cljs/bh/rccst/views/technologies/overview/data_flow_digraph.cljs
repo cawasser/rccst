1fd617c2005c7b36911471719333e2aac7159367
Chris Wasser <cawasser@mac.com>
2022-05-29 15:11:48 -0400

    - added more on the data-flow design approach

--- src/cljs/bh/rccst/views/technologies/overview/data_flow_digraph.cljs
+++ src/cljs/bh/rccst/views/technologies/overview/data_flow_digraph.cljs
@@ -1,24 +1,59 @@
 (ns bh.rccst.views.technologies.overview.data-flow-digraph
-  (:require [woolybear.ad.layout :as layout]
-            [woolybear.packs.flex-panel :as flex]))
+  (:require [woolybear.ad.layout :as layout]))
 
 
 (defn page []
   [layout/page {:extra-classes :is-fluid}
-   ;[flex/flex-panel {:extra-classes :is-fluid
-   ;                  :height        "76vh"}
-   ; [flex/flex-top {:extra-classes :is-fluid}
-     [:div.is-fluid
-      [:h2.has-text-info "Data-flow Oriented Design"]
-      [layout/markdown-block
-       "> _a system design approach where data is transformed across a collection of 'processes' (nodes)
-> that are connected (edges) via 'channels' (pub/sub, topics, queues, function calls, etc.)_
+   [:div.is-fluid
+    [:h2.has-text-info "Data-flow Oriented Design"]
+    [layout/markdown-block
+     "> _a system design approach where data is transformed across a collection of 'processes',
+> sometimes called 'automata' (nodes) that are connected (edges) via 'channels' (pub/sub, topics,
+> queues, function calls, etc.)_
 
 We have developed a mental model of software as a directed graph of data transformations. We see this
 model as applicable at al levels of system design (requirements, architecture, modularity, deployment)
-and in all aspects of system implementation (front-end, back-end, etc.)
+and in all aspects of system implementation (front-end, back-end, etc.).
 
-### Types of transforms:
+This model fits perfectly within the idiomatic approach promoted by the Clojure functional programing language
+we are developing in: transformations of immutable data, glued together into systems. Rich Hickey, creator of Clojure,
+strongly promotes the use of queue as a system implementation mechanism (see [here](https://www.youtube.com/watch?v=LKtk3HCgTa8) and
+[here](https://www.youtube.com/watch?v=ROor6_NGIWU)) to decouple the elements of computation/transformation.
+
+As can be seen through a survey of dataflow system approaches, this model is often applied only to the 'services' portion, often called
+the 'back-end', but we have determined that this approach is perfectly amenable to developing User Interfaces (UIs), or 'front-ends'.
+This is a significant shift form how UI development if typically taught and practiced.
+
+### How we got here.
+
+
+The breakthrough came when we recognized that 'molecules', as introduced by Brad Frost as part of his [Atomic Design](),
+in chemistry classes are physically modeled using balls and sticks, as show in Figure 1.
+
+![](/imgs/data-flow/chemical-modeling-kit.jpeg)
+
+_Figure 1. Molecule Modeling Kit_
+
+Combined with Clojure's, and most other functional programming languages', idiomatic approach promoting system composition from
+transformations of data (using map, filter, and reduce), and the Re-frame notion of the [_signal graph_](https://day8.github.io/re-frame/a-loop/#domino-4-query)
+we saw that by placing the Signal Graph center of the design, rather than being a by product of a more typical approach, we could unlock
+the power of the directed graph, simplifying both design and implementation. And standarizing UI developer, rarher than perpetuations
+the _ad hoc_ approach commonly used.
+
+We could implement any UI in a standard way, using a standard software mechanism, rather than continually developing one-off, custom
+UI elements and composed widgets.
+
+This approach also means we can apply the same System Engineering approach used for the services on the back-end to the
+front-end as well! In fact, the front-end is not really a separate system, as it is typically considered in the ad hoc approach, but
+just one more collection of transformations of data within the boundary of the 'system' as a whole. So, instead of depositing
+the results of a transformation into a database or a channel, queue, or topic, the results are converted one last time into HTML
+and deposited on the User's display!
+
+
+### Transforming Data
+
+
+#### Types of transforms:
 
 - filter : select only certain items or just parts of items, based upon some criterion.
 - transform : convert the physical format; e.g., vector of hash-maps to vector of vectors, etc.
@@ -39,6 +74,7 @@ of a microservice)
 - [Stream processing](https://en.wikipedia.org/wiki/Stream_processing)
 - [Reactive programming](https://en.wikipedia.org/wiki/Reactive_programming)
 - [LabView](https://www.ni.com/getting-started/labview-basics/dataflow)
+- [Power BI](https://en.wikipedia.org/wiki/Microsoft_Power_BI)
 - [Lucid programming language](https://en.wikipedia.org/wiki/Lucid_(programming_language))
 - [Petri Nets](https://en.wikipedia.org/wiki/Petri_net)
 - [React](https://reactjs.org/)
@@ -61,7 +97,6 @@ any of the inputs change.
 
 - [Data Flow (Wikipedia)](https://en.wikipedia.org/wiki/Dataflow)
 - [Dataflow programming (Wikipedia)](https://en.wikipedia.org/wiki/Dataflow_programming)
--
 - [Kahn process network (Wikipedia)](https://en.wikipedia.org/wiki/Kahn_process_networks)
 
 "]]])
