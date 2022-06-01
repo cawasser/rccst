ed6950127f8474c2f6d9e457660f6c659fe94dc9
Chris Wasser <cawasser@mac.com>
2022-06-01 13:53:28 -0400

    - minor tweaks to the data-table atom as we start interoperating with "fixed-data-table-2"

--- src/cljs/bh/rccst/ui_component/atom/bh/data_table.cljs
+++ src/cljs/bh/rccst/ui_component/atom/bh/data_table.cljs
@@ -14,23 +14,21 @@
 
 (def sample-data ex/tabular-data)
 
-(def data ["string 1" "string 2" "string 3"])
+(def sample-data-2 ["string 1" "string 2" "string 3"])
 
 
 (defn table [& {:keys [data config-data]}]
 
   (let [d (h/resolve-value data)
         c (h/resolve-value config-data)]
-    [:> Table {:rowHeight 50 :rowsCount 0 :width 500 :height 500 :headerHeight 50}
+
+    [:> Table {:rowHeight 50 :rowsCount (count sample-data-2) :width 500 :height 500 :headerHeight 50}
      [:> Column {:header (fn [] (r/as-element [:> DataCell "Col 1"]))
                  :cell (fn [] (r/as-element [:> DataCell "Column 1 static content"]))
                  :width 200}]
      [:> Column {:header (fn [] (r/as-element [:> DataCell "Col 2"]))
                  :cell (fn [idx _]
                         (let [{:keys [rowIndex]} (js->clj idx :keywordize-keys true)]
-                         (r/as-element [:> DataCell (nth data rowIndex)])))
-                 :width 200}
-       [:div "data table"
-        [:p (str @d)]
-        [:p (str @c)]]]]))
+                         (r/as-element [:> DataCell (nth sample-data-2 rowIndex)])))
+                 :width 200}]]))
 
