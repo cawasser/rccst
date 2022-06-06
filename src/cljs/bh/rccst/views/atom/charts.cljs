fc9bcad14584da2df8db3af6c199117b6652bb03
Chris Wasser <cawasser@mac.com>
2022-06-06 14:24:43 -0400

    - pulled the rest of the chart examples into the single page approach

--- src/cljs/bh/rccst/views/atom/charts.cljs
+++ src/cljs/bh/rccst/views/atom/charts.cljs
@@ -1,29 +1,21 @@
 (ns bh.rccst.views.atom.charts
-  (:require [bh.rccst.events :as events]
-            [bh.rccst.ui-component.tabbed-panel :as tabbed-panel]
-            [bh.rccst.views.atom.example.chart.area-chart :as area-chart]
+  (:require [bh.rccst.views.atom.example.chart.area-chart :as area-chart]
             [bh.rccst.views.atom.example.chart.bar-chart :as bar-chart]
-            [bh.rccst.views.atom.example.chart.radial-bar-chart :as radial-bar-chart]
-            [bh.rccst.views.atom.example.chart.pie-chart :as pie-chart]
             [bh.rccst.views.atom.example.chart.colored-pie-chart :as colored-pie-chart]
-            [bh.rccst.views.atom.example.chart.line-chart :as line-chart]
             [bh.rccst.views.atom.example.chart.funnel-chart :as funnel-chart]
+            [bh.rccst.views.atom.example.chart.line-chart :as line-chart]
+            [bh.rccst.views.atom.example.chart.pie-chart :as pie-chart]
+            [bh.rccst.views.atom.example.chart.radar-chart :as radar-chart]
+            [bh.rccst.views.atom.example.chart.radial-bar-chart :as radial-bar-chart]
             [bh.rccst.views.atom.example.chart.sankey-chart :as sankey-chart]
             [bh.rccst.views.atom.example.chart.scatter-chart :as scatter-chart]
-            [bh.rccst.views.atom.example.chart.radar-chart :as radar-chart]
             [bh.rccst.views.atom.example.chart.treemap-chart :as treemap-chart]))
 
 
-(declare examples)
+(defn page
+  "Charts"
+  []
 
-
-(def navbar [[:charts/charts "Charts" [examples]]
-
-             [:charts/scatter-chart "Scatter" [scatter-chart/examples]]
-             [:charts/treemap-chart "TreeMap" [treemap-chart/examples]]])
-
-
-(defn- examples []
   [:div
    [area-chart/examples]
    [bar-chart/examples]
@@ -33,16 +25,7 @@
    [pie-chart/examples]
    [radar-chart/examples]
    [radial-bar-chart/examples]
-   [sankey-chart/examples]])
+   [sankey-chart/examples]
+   [scatter-chart/examples]
+   [treemap-chart/examples]])
 
-
-(defn page
-  "Charts"
-  []
-
-  [tabbed-panel/tabbed-panel
-   :title ""
-   :short-name "charts"
-   :description ""
-   :children navbar
-   :start-panel :charts/charts])
